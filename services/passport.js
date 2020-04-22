fbd7631bf10023d16d94d0aa04248e2b923eeafb
oasis10702 <oasis10702@gmail.com>
2020-04-22 14:44:48 +0900

    save user id to user collection

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -13,6 +16,8 @@ passport.use(
       console.log('accessToken', accessToken);
       console.log('refreshToken', refreshToken);
       console.log('profile:', profile);
+
+      new User({ googleId: profile.id }).save();
     }
   )
 );
